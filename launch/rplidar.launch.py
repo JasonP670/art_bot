cfe1435db4e737a128094f5fb1c2003b77443286
Jason P <my@email.com>
2023-10-17 21:06:04 -0600

    move instructions, make pi's serial port default

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -1,3 +1,9 @@
+# I had to run the following commands in the terminal to be able to connect to the lidar
+# sudo adduser $USER dialout
+# sudo chmod 666 /dev/ttyUSB0 
+# This has to be done each time a session is started, unless you make a udev rule
+
+
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
@@ -9,8 +15,8 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/serial/by-path/pci-0000:00:1a.0-usb-0:1.2:1.0-port0',
-                # 'serial_port': '/dev/ttyUSB0',
+                # 'serial_port': '/dev/serial/by-path/pci-0000:00:1a.0-usb-0:1.2:1.0-port0',
+                'serial_port': '/dev/ttyUSB0',
                 'frame_id': 'laser_frame',
                 'angle_compensate': True,
                 'scan_mode': 'Standard'
